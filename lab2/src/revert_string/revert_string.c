8430f97faf462bc231e9d45ea2531272d6641901
Kirillabaskin <kirillabaskin99@gmail.com>
2019-03-06 18:36:38 +0000

    задание 2 лаб2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <stdio.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int n=0;
+	while(str[n]!='\0') {
+	    n++;
+	}
+	char swap;
+	for(int i=0;i<n/2;i++){
+	    swap=str[i];
+	    str[i]=str[n-i-1];
+	    str[n-i-1]=swap;
+	}
 }
 
